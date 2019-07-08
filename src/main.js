4faa4ce272e977228f09b8ea5823c0d8fb6f2349
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-07-08 16:16:51 -0500

    Agregando estilos en css y uniendo las dos ramas, funcion para filtrar por tipo, funcion para filtrar de mayor a menor, menor a mayor, filtrar por debilidades, html semantico con grid

--- src/main.js
+++ src/main.js
@@ -6,106 +6,82 @@ const selectWeaknesses = document.getElementById('weaknesses');
 // let allData = data;
 
 //FUNCION PARA IMPRIMIR DATA EN TARJETAS
-const printData = (data)=>{ 
-    // console.log(data)
+const printData = (data)=>{
+   // console.log(data)
 let str = ''
-    data.forEach(element => { //Ejecuta la funcion una vez por cada elemento
-         str += `
-         
-         <div class="card col-3">
-         <div class="card-image"><img src="${element.img}"></img></div>
-         <div class="card-text">
-         <p><strong>${element.name}</strong></p>
-         <p>${element.num}</p>
-         <p> Tipo: ${element.type[0]} , ${element.type[1]}</p>
-          
-         </div>
-         </div>`
-
+   data.forEach(element => { //Ejecuta la funcion una vez por cada elemento
+        str += `<div class="card col-3">
+        <div class="card-image"><img src="${element.img}"></img></div>
+        <div class="card-text">
+        <p><strong>${element.name}</strong></p>
+        <p>Probabilidad: ${element.spawn_chance} </p>
+        <p>${element.num}</p>
+        <p> Tipo: ${element.type[0]} , ${element.type[1]}</p>        
+        </div>
+        </div>`    
     });
-    card.innerHTML = str;
+   card.innerHTML = str;
+
 };
 
 //FUNCION PARA FILTRAR POR TIPO
 let filterType = (ev) => {
-    const typeValue = ev.target.value; //Guardando el valor del selector de tipo
-    let filterTypeSelect= data;
-    console.log(data)
-    if (typeValue==="all-types"){
-        printData(POKEMON.pokemon );
-        console.log(filterTypeSelect);
-    }
-    else { 
-     filterTypeSelect = window.dataManager.filterByType(data,typeValue); //llamando la funcion desde el objeto global window para filtrar tipos
-    printData(filterTypeSelect); // Reutilizando la funcion para imprimir por tipo sobre las tarjetas
-    }
+   const typeValue = ev.target.value; //Guardando el valor del selector de tipo
+   let filterTypeSelect= data;
+   console.log(data)
+   if (typeValue==="all-types"){
+       printData(POKEMON.pokemon);
+       console.log(filterTypeSelect);
+   }
+   else {
+    filterTypeSelect = window.dataManager.filterByType(data,typeValue); //llamando la funcion desde el objeto global window para filtrar tipos
+   printData(filterTypeSelect); // Reutilizando la funcion para imprimir por tipo sobre las tarjetas
+   }
 };
-
-selectType.addEventListener("change",filterType); // Dandole evento change al selector 
-
-//FUNCION PARA FILTRA LA PROBABILIDAD DE MAYOR A MENOR
+selectType.addEventListener("change",filterType); // Dandole evento change al selector//FUNCION PARA FILTRA LA PROBABILIDAD DE MAYOR A MENOR
 let filterProbability = (ev)=>{
-    const probabilityValue = ev.target.value;//Guardando el valor del selector de probabilidad
-    let filterProbabilitySelect = data;
-
-    if (probabilityValue === 'all-probabilities'){
-        filterProbabilitySelect = printData(filterProbabilitySelect);
-        console.log(probabilityValue);
-    }
-    else{
-    filterProbabilitySelect = window.dataManager.filterByProbabilities(data,probabilityValue);
-    printData(filterProbabilitySelect);
+   const probabilityValue = ev.target.value;//Guardando el valor del selector de probabilidad
+   let filterProbabilitySelect = data;    if (probabilityValue === 'all-probabilities'){
+       filterProbabilitySelect = printData(filterProbabilitySelect);
+       console.log(probabilityValue);
+   }
+   else{
+   filterProbabilitySelect = window.dataManager.filterByProbabilities(data,probabilityValue);
+   printData(filterProbabilitySelect);
+   }
+}
+selectProbability.addEventListener("change",filterProbability);//console.log(selectProbability);//FUNCIÓN PARA FILTRAR POR DEBILIDADESlet filterWeaknesses =(ev)=>{
+let filterWeaknesses =(ev)=>{  
+const weaknessesValue = ev.target.value;
+   let filterWeaknesSelect= data;
+   if(weaknessesValue=="all-weaknesses"){
+       filterWeaknesSelect= printData(filterWeaknesSelect);
+   }
+   else{
+   filterWeaknessesSelect = window.dataManager.filterByWeaknes(data,weaknessesValue);
+   console.log(weaknessesValue);
+       printData(filterWeaknessesSelect);
     }
 }
-selectProbability.addEventListener("change",filterProbability);
-
-//console.log(selectProbability);
-
-//FUNCIÓN PARA FILTRAR POR DEBILIDADES
-
-let filterWeaknesses =(ev)=>{
-    const weaknessesValue = ev.target.value;
-    let filterWeaknesSelect= data;
-    if(weaknessesValue=="all-weaknesses"){
-        filterWeaknesSelect= printData(filterWeaknesSelect);
-    }
-    else{
-    filterWeaknessesSelect = window.dataManager.filterByWeaknes(data,weaknessesValue);
-    console.log(weaknessesValue);
-        printData(filterWeaknessesSelect);
-}
-}
-selectWeaknesses.addEventListener("change",filterWeaknesses);
-
-
-printData(data);
+selectWeaknesses.addEventListener("change",filterWeaknesses);printData(data);
 
 
 
-
+printData(data);//pinta toda la data.
 
 //FUNCION PRUEBA PARA FILTRAR
 // const grassPokemon= data.filter(function(element){
 //     console.log(element.type);
 //     return (element.type[0]=='Grass');
 //  });
-// console.log(grassPokemon);
-
-    
-
-//PRUEBA PARA PINTAR DATA EN HTML
-// for(let i =0; i<POKEMON.pokemon.length;i++){ 
+// console.log(grassPokemon);//PRUEBA PARA PINTAR DATA EN HTML
+// for(let i =0; i<POKEMON.pokemon.length;i++){
 // let cards = `<div id="pokemon-card" class="pokemon-card" >
 //     <img src="${element.image}"></img>
 //     <p>${element.num}</p>
 //     <p>${element.name}</p>
 //     <p>Tipo: ${element.type}</p>
-//     <p>Dulces para evolucionar ${element.candy_count}</p> </div>`;
-
-//     printCards.insertAsJacentHTML('afterbegin',cards)
-//     return print;
-
-//     const root = document.getElementById('pokemon-card');
+//     <p>Dulces para evolucionar ${element.candy_count}</p> </div>`;//     printCards.insertAsJacentHTML('afterbegin',cards)
+//     return print;//     const root = document.getElementById('pokemon-card');
 //     cards += root.insertAdjacentHTML('afterbegin', cards);
-// }  
-
+// }
