8012ce5a6715ef079cdc0597a1bac31fb03022cd
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-08-21 18:29:36 -0500

    agregando la promesa para consumir el json, dentro de la promesa se agrego la constante data y los detonadores de los cambios que antes estaban fuera de la promesa, tambien a los detonadores se le agrego un evento y al evento se le agrego un callback de las funciones que llaman a las funciones que se encuentran en el archivo data.js respectivamente.... tambien se le agrego como para parametro la data a cada una de las funciones que guardan el valor del selector... el fetch se posiciono al final del archivo main.js

--- src/main.js
+++ src/main.js
@@ -1,70 +1,75 @@
-const data = window.POKEMON.pokemon;
 const card = document.getElementById('content-card');
 const selectType = document.getElementById("filter-type");
 const selectProbability = document.getElementById('probability');
 const selectWeaknesses = document.getElementById('weaknesses');
 const curiousFactContainer = document.getElementById('curious-fact-container');
 
-//FUNCION PARA IMPRIMIR DATA EN TARJETAS
-const printData = (data)=>{
-    let str = '';
-    data.forEach(element => { //Ejecuta la funcion una vez por cada elemento    
+//LLAMDA A LA FUNCION IMPRIMIR DATA EN TARJETAS
+const printData = (data) => {
+  let str = '';
+  data.forEach(element => { //Ejecuta la funcion una vez por cada elemento    
     str += `<div class="card col-3">
-    <img src="${element.img}" width=100% >
-    <p><strong>${element.name}</strong></p>
-    <p>${element.num}</p>
-    <p>Probabilidad: ${element.spawn_chance} % </p>
-    <p> Tipo: ${element.type[0]} , ${element.type[1]}</p>        
-    </div>`;   
-});
-    card.innerHTML = str;
-    const totalKmOfEgg = window.dataManager.computeStats(data);
-      curiousFactContainer.innerHTML = 
-      '<br><p><strong>Dato Curioso</p></strong><br><p>Total de Km por recorrer:</p>' + totalKmOfEgg;
+        <img src="${element.img}" width=100% >
+        <p><strong>${element.name}</strong></p>
+        <p>${element.num}</p>
+        <p>Probabilidad: ${element.spawn_chance} % </p>
+        <p> Tipo: ${element.type[0]} , ${element.type[1]}</p>        
+        </div>`;
+  });
+  card.innerHTML = str;
+
+  // LLAMANDO A LA FUNCION DE ESTADISTICA KM POR RECORRER
+  const totalKmOfEgg = window.dataManager.computeStats(data);
+  curiousFactContainer.innerHTML =
+    '<br><p><strong>Dato Curioso</p></strong><br><p>Total de Km por recorrer:</p>' + totalKmOfEgg;
 };
 
-//FUNCION PARA FILTRAR POR TIPO
-let filterType = (ev) => {
-   const typeValue = ev.target.value; //Guardando el valor del selector de tipo
-   let filterTypeSelect= data;
-   //console.log(data);
-   if (typeValue==="all-types"){
-       printData(POKEMON.pokemon);
-       //console.log(filterTypeSelect);
-   }
-   else {
-    filterTypeSelect = window.dataManager.filterByType(data,typeValue); //llamando la funcion desde el objeto global window para filtrar tipos
-   printData(filterTypeSelect); // Reutilizando la funcion para imprimir por tipo sobre las tarjetas
-   }
+//LLAMADA A LA FUNCION FILTRAR POR TIPO
+let filterType = (ev, data) => {
+  const typeValue = ev.target.value; //Guardando el valor del selector de tipo
+  let filterTypeSelect = data;
+  if (typeValue === "all-types") {
+    printData(POKEMON.pokemon);
+  } else {
+    filterTypeSelect = window.dataManager.filterByType(data, typeValue); //llamando la funcion desde el objeto global window para filtrar tipos
+    printData(filterTypeSelect); // Reutilizando la funcion para imprimir por tipo sobre las tarjetas
+  }
 };
-selectType.addEventListener("change",filterType); // Dandole evento change al selector//FUNCION PARA FILTRA LA PROBABILIDAD DE MAYOR A MENOR
-let filterProbability = (ev)=>{
-   const probabilityValue = ev.target.value;//Guardando el valor del selector de probabilidad
-   const probabilityOfFinding = 'spawn_chance';//Guardando la probabilidad de encontrar
-   console.log(probabilityValue);
-   console.log(probabilityOfFinding);
-   if (probabilityValue === 'all-probabilities'){
-       printData(data);
-       //console.log(probabilityValue);
-   } else{
+
+// LLAMADA A LA FUNCION ORDENAR POR PROBABILIDAD
+let filterProbability = (ev, data) => {
+  const probabilityValue = ev.target.value; //Guardando el valor del selector de probabilidad
+  const probabilityOfFinding = 'spawn_chance'; //Guardando la probabilidad de encontrar
+  if (probabilityValue === 'all-probabilities') {
+    printData(data);
+  } else {
     const filteredProbabilitySelect = window.dataManager.filterByProbabilities(data, probabilityOfFinding, probabilityValue);
-    console.log(filteredProbabilitySelect);
     printData(filteredProbabilitySelect);
-   }
+  }
 };
-selectProbability.addEventListener("change",filterProbability);//console.log(selectProbability);//FUNCIÓN PARA FILTRAR POR DEBILIDADESlet filterWeaknesses =(ev)=>{
-let filterWeaknesses =(ev)=>{  
-const weaknessesValue = ev.target.value;
-   let filterWeaknesSelect= data;
-   if(weaknessesValue=="all-weaknesses"){
-       filterWeaknesSelect= printData(filterWeaknesSelect);
-   }
-   else{
-    filterWeaknesSelect = window.dataManager.filterByWeaknes(data,weaknessesValue);
-   //console.log(weaknessesValue);
-       printData(filterWeaknesSelect);
-    }
-};
-selectWeaknesses.addEventListener("change",filterWeaknesses);
 
-printData(data);//pinta toda la data.
+//LLAMADA A LA FUNCION FILTRAR POR DEBILIDADES
+let filterWeaknesses = (ev, data) => {
+  const weaknessesValue = ev.target.value;
+  let filterWeaknesSelect = data;
+  if (weaknessesValue == "all-weaknesses") {
+    filterWeaknesSelect = printData(filterWeaknesSelect);
+  } else {
+    filterWeaknesSelect = window.dataManager.filterByWeaknes(data, weaknessesValue);
+    printData(filterWeaknesSelect);
+  }
+};
+
+//FETCH PARA CONSUMIR DATA DEL JSON
+fetch('./data/pokemon/pokemon.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then(obj => {
+    const data = obj.pokemon;
+    selectType.addEventListener("change", (event) => filterType(event, data)); // El selector escucha el cambio y ejecuta el evento, este evento recibe como callback la funcion para cachar el valor y llama función para filtrar por tipo
+    selectProbability.addEventListener("change", (event) => filterProbability(event, data));
+    selectWeaknesses.addEventListener("change", (event) => filterWeaknesses(event, data));
+    printData(data); //pinta toda la data.
+  })
+  .catch(() => console.error(err))
